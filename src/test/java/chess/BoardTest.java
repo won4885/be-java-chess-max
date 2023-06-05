78a9add3052b3167cff8051e90f20a1851e3dcf4
Sully <2dcoder@naver.com>
2023-06-05 16:24:47 +0900

    test(Board): 체스 판 초기화 테스트 틀 생성
    TDD 기반
    
    Board 클래스에 initialize() 메소드는 흰색 Pawn과 검은색 Pawn을 생성해 저장하는 List를 추가해 구현할 수 있다.
    
    BoardTest의 initialize() 메소드에서 흰색 Pawn 열의 결과가 "pppppppp" 인지 검증하고, 검은색 Pawn 열의 결과가 "PPPPPPPP"인지를 검증한다.
    
    StringBuilder를 활용해 ArrayList에 담겨있는 Pawn의 값을 합친다.

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,31 +1,16 @@
 package chess;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pieces.Pawn;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static pieces.PieceColor.BLACK_COLOR;
-import static pieces.PieceColor.WHITE_COLOR;
 
 class BoardTest {
-    @DisplayName("Pawn을 체스 판에 추가할 수 있다.")
     @Test
-    void create() {
+    void initialize() throws Exception {
         final Board board = new Board();
+        board.initialize();
 
-        // white
-        final Pawn white = new Pawn(WHITE_COLOR);
-        board.add(white);
-
-        assertThat(1).isEqualTo(board.size());
-        assertThat(white).isEqualTo(board.findPawn(0));
-
-        // black
-        final Pawn black = new Pawn(BLACK_COLOR);
-        board.add(black);
-
-        assertThat(2).isEqualTo(board.size());
-        assertThat(black).isEqualTo(board.findPawn(1));
+        assertThat("pppppppp").isEqualTo(board.getWhitePawnsResult());
+        assertThat("PPPPPPPP").isEqualTo(board.getBlackPawnsResult());
     }
 }
