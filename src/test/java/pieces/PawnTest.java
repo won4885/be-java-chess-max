505d76bf95299ac6f4f97f7d63035943732fb729
Sully <2dcoder@naver.com>
2023-06-05 16:13:10 +0900

    test(Pawn): Pawn 색에 따른 출력 문자 부여 테스트 틀 생성
    TDD 기반
    
    체스 판의 기물을 출력하는 기능을 구현하려 한다.
    
    검은색 Pawn 출력은 대문자 'P'이고, 흰색 Pawn의 출력은 소문자 'p'를 출력하는 방식으로 구현하려 한다.
    
    Pawn을 생성할 때 두 번째 인자로 출력할 값을 전달해 문제를 해결한다.
    
    다른 방법으로 구현할 수 있는지 검토해 본다.

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -4,28 +4,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static pieces.PieceColor.BLACK_PIECE;
-import static pieces.PieceColor.WHITE_PIECE;
+import static pieces.PieceColor.*;
 
 class PawnTest {
     @DisplayName("흰색 폰과 검은색 폰만이 생성되어야 한다")
     @Test
     void create() {
-        final Pawn pawn1 = new Pawn(WHITE_PIECE);
-        final Pawn pawn2 = new Pawn(BLACK_PIECE);
+        validatePawn(WHITE_COLOR, WHITE_REPRESENTATION);
+        validatePawn(BLACK_COLOR, BLACK_REPRESENTATION);
+    }
 
-        assertAll(() -> assertThat(pawn1.getColor()).isEqualTo(WHITE_PIECE),
-                () -> assertThat(pawn2.getColor()).isEqualTo(BLACK_PIECE),
-                () -> assertThrows(IllegalArgumentException.class,
-                        () -> assertThat(new Pawn("orange"))));
+    private void validatePawn(final String color, final char representation) {
+        final Pawn pawn = new Pawn(color, representation);
+        assertThat(color).isEqualTo(pawn.getColor());
+        assertThat(representation).isEqualTo(pawn.getRepresentation());
     }
 
     @DisplayName("색이 없는 폰을 생성하는 경우 흰색 말을 생성한다")
     @Test
     void create_기본생성자() {
         final Pawn pawn = new Pawn();
-        assertThat(WHITE_PIECE).isEqualTo(pawn.getColor());
+        assertThat(WHITE_COLOR).isEqualTo(pawn.getColor());
+        assertThat(WHITE_REPRESENTATION, pawn.getRepresentation());
     }
 }
