de3220773c0c6e4c544b8fbc9699615c027b453b
Sully <2dcoder@naver.com>
2023-06-05 15:26:27 +0900

    refactor(Pawn): 테스트 코드에도 상수 클래스 적용

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -7,16 +7,18 @@ import pieces.pawn.Pawn;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static pieces.pawn.PawnColor.BLACK_PAWN;
+import static pieces.pawn.PawnColor.WHITE_PAWN;
 
 class PawnTest {
     @DisplayName("흰색 폰과 검은색 폰만이 생성되어야 한다")
     @Test
     void create() {
-        final Pawn pawn1 = new Pawn("white");
-        final Pawn pawn2 = new Pawn("black");
+        final Pawn pawn1 = new Pawn(WHITE_PAWN);
+        final Pawn pawn2 = new Pawn(BLACK_PAWN);
 
-        assertAll(() -> assertThat(pawn1.getColor()).isEqualTo("white"),
-                () -> assertThat(pawn2.getColor()).isEqualTo("black"),
+        assertAll(() -> assertThat(pawn1.getColor()).isEqualTo(WHITE_PAWN),
+                () -> assertThat(pawn2.getColor()).isEqualTo(BLACK_PAWN),
                 () -> assertThrows(IllegalArgumentException.class,
                         () -> assertThat(new Pawn("orange"))));
     }
@@ -25,6 +27,6 @@ class PawnTest {
     @Test
     public void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
-        assertThat("white").isEqualTo(pawn.getColor());
+        assertThat(WHITE_PAWN).isEqualTo(pawn.getColor());
     }
 }
