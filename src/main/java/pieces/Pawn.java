cea53fc3c3e53012902263ab195308539d3873f9
Sully <2dcoder@naver.com>
2023-06-05 15:08:48 +0900

    feat(Pawn): while와 black의 색만 생성되도록 구현
    나머지 색이 생성자를 통해 들어온다면 예외를 던지게끔 함
    테스트 코드도 마찬가지로 수정

---
+++ src/main/java/pieces/Pawn.java
@@ -0,0 +1,23 @@
+package pieces;
+
+import lombok.Getter;
+
+@Getter
+public class Pawn {
+    private final String color;
+
+    public Pawn(String color) {
+        validateColor(color);
+        this.color = color;
+    }
+
+    /**
+     * black 또는 while 색상이 아닌 경우 예외를 던짐
+     * TODO: 던진 예외를 처리해야 함
+     */
+    private void validateColor(String color) {
+        if (!color.equals("black") && !color.equals("white")) {
+            throw new IllegalArgumentException("[ERROR] black 또는 white 폰을 입력해야 합니다. color: " + color);
+        }
+    }
+}
--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PawnTest {
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("흰색 폰과 검은색 폰만이 생성되어야 한다")
     @Test
     void create() {
-        Pawn pawn = new Pawn("while");
-        assertThat(pawn.getColor()).isEqualTo("while");
+        Pawn pawn1 = new Pawn("white");
+        Pawn pawn2 = new Pawn("black");
+
+
+        assertAll(() -> assertThat(pawn1.getColor()).isEqualTo("white"),
+                () -> assertThat(pawn2.getColor()).isEqualTo("black"),
+                () -> assertThrows(IllegalArgumentException.class,
+                        () -> assertThat(new Pawn("orange"))));
     }
 }
