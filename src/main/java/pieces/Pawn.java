afc8a17ff056f1658d59aa1227de917bca294ec4
Sully <2dcoder@naver.com>
2023-06-05 16:16:10 +0900

    rename: validatePawnColor -> validatePawn
    원래는 Color만 검증했는데 이제는 표현법까지 검증하니 리네이밍

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -14,7 +14,7 @@ public class Pawn {
     }
 
     public Pawn(final String color) {
-        validatePawnColor(color);
+        validatePawn(color);
         this.color = color;
     }
 
@@ -22,7 +22,7 @@ public class Pawn {
      * black 또는 white 색상이 아닌 경우 예외를 던짐
      * TODO: 던진 예외를 처리해야 함
      */
-    private void validatePawnColor(final String color) {
+    private void validatePawn(final String color) {
         if (!color.equals(BLACK_COLOR) && !color.equals(WHITE_COLOR)) {
             throw new IllegalArgumentException("[ERROR] black 또는 white 폰을 입력해야 합니다. color: " + color);
         }
