d232fbb7b842348e9aee0fe44ecd1aec34ca6a14
Sully <2dcoder@naver.com>
2023-06-05 16:51:02 +0900

    feat(Board): 체스 판을 p와 P로 구분하여 초기화하는 기능 구현
    whitePawns와 blackPawns로 구분하였다.
    객체로 생성해주는 게 되게 재밌다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -5,21 +5,63 @@ import pieces.Pawn;
 import java.util.ArrayList;
 import java.util.List;
 
+import static pieces.PieceColor.BLACK_COLOR;
+import static pieces.PieceColor.BLACK_REPRESENTATION;
+
 public class Board {
-    private final List<Pawn> board = new ArrayList<>();
+    private static final int BOARD_LENGTH = 8;
+    private final List<Pawn> whitePawns = new ArrayList<>();
+    private final List<Pawn> blackPawns = new ArrayList<>();
 
     public Board() {
     }
 
-    public void add(final Pawn pawn) {
-        board.add(pawn);
+    public void addWhitePawns(final Pawn pawn) {
+        whitePawns.add(pawn);
     }
 
-    public int size() {
-        return board.size();
+    public void addBlackPawns(final Pawn pawn) {
+        blackPawns.add(pawn);
     }
 
-    public Pawn findPawn(final int index) {
-        return board.get(index);
+    public int sizeOfWhitePawns() {
+        return whitePawns.size();
+    }
+
+    public int sizeOfBlackPawns() {
+        return blackPawns.size();
+    }
+
+    public Pawn findWhitePawn(final int index) {
+        return whitePawns.get(index);
+    }
+
+    public Pawn findBlackPawn(final int index) {
+        return blackPawns.get(index);
+    }
+
+    public void initialize() {
+        for (int i = 0; i < BOARD_LENGTH; i++) {
+            addWhitePawns(new Pawn());
+            addBlackPawns(new Pawn(BLACK_COLOR, BLACK_REPRESENTATION));
+        }
+    }
+
+    public String getWhitePawnsResult() {
+        StringBuilder white = new StringBuilder();
+        for (Pawn whitePawn : whitePawns) {
+            white.append(whitePawn.getRepresentation());
+        }
+
+        return white.toString();
+    }
+
+    public String getBlackPawnsResult() {
+        StringBuilder black = new StringBuilder();
+        for (Pawn blackPawn : blackPawns) {
+            black.append(blackPawn.getRepresentation());
+        }
+
+        return black.toString();
     }
 }
--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,10 +1,12 @@
 package chess;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BoardTest {
+    @DisplayName("initialize: 체스 판을 p와 P로 구분하여 초기화한다.")
     @Test
     void initialize() throws Exception {
         final Board board = new Board();
