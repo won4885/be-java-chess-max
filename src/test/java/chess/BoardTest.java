df41b3e9f3a4961dc550e6786c5fb4d35ad8c7bf
Sully <2dcoder@naver.com>
2023-06-07 15:26:18 +0900

    test(Board): 전체 기물의 상태를 볼 수 있는 체스판 구현 및 테스트 틀 생성
    TDD 기반

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,18 +1,44 @@
 package chess;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static utils.StringUtils.appendNewLine;
 
 class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
+
     @DisplayName("initialize: 체스 판을 p와 P로 구분하여 초기화한다.")
     @Test
     void initialize() throws Exception {
+
         final Board board = new Board();
         board.initialize();
 
         assertThat("pppppppp").isEqualTo(board.getWhitePawnsResult());
         assertThat("PPPPPPPP").isEqualTo(board.getBlackPawnsResult());
+    }
+
+    @Test
+    void create() throws Exception {
+        board.initialize();
+
+        assertThat(32).isEqualTo(board.pieceCount());
+
+        String blankRank = appendNewLine("........");
+
+        assertThat(board.showBoard()).isEqualTo(
+                appendNewLine("RNBQKBNR") +
+                appendNewLine("PPPPPPPP") +
+                blankRank + blankRank + blankRank + blankRank +
+                appendNewLine("pppppppp") +
+                appendNewLine("rnbqkbnr"));
     }
 }
