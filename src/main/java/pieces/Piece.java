f9cced7a03c871c091fbb4b23ffd5d673a03fc1a
Sully <2dcoder@naver.com>
2023-06-08 17:26:57 +0900

    refactor(Piece): Color를 상수에서 enum 사용으로 변경

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -4,8 +4,19 @@ import lombok.Getter;
 
 import java.util.Objects;
 
+import static pieces.Piece.Color.BLACK;
+import static pieces.Piece.Color.WHITE;
+
 @Getter
 public class Piece {
+    public enum Color {
+        WHITE, BLACK, NOCOLOR;
+    }
+
+    public enum Type {
+        PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING, NO_PIECE;
+    }
+
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
     public static final char WHITE_PAWN_REPRESENTATION = 'p';
@@ -21,17 +32,17 @@ public class Piece {
     public static final char WHITE_KING_REPRESENTATION = 'k';
     public static final char BLACK_KING_REPRESENTATION = 'K';
 
-    private final String color;
+    private final Color color;
     private final char representation;
 
     /**
      * 기본 생생자는 white 기반
      */
     private Piece() {
-        this(WHITE_COLOR, WHITE_PAWN_REPRESENTATION);
+        this(WHITE, WHITE_PAWN_REPRESENTATION);
     }
 
-    private Piece(final String color, final char representation) {
+    private Piece(final Color color, final char representation) {
         validatePiece(color, representation);
         this.color = color;
         this.representation = representation;
@@ -42,63 +53,63 @@ public class Piece {
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(BLACK_COLOR, BLACK_PAWN_REPRESENTATION);
+        return new Piece(BLACK, BLACK_PAWN_REPRESENTATION);
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(WHITE_COLOR, WHITE_KNIGHT_REPRESENTATION);
+        return new Piece(WHITE, WHITE_KNIGHT_REPRESENTATION);
     }
 
     public static Piece createBlackKnight() {
-        return new Piece(BLACK_COLOR, BLACK_KNIGHT_REPRESENTATION);
+        return new Piece(BLACK, BLACK_KNIGHT_REPRESENTATION);
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(WHITE_COLOR, WHITE_ROOK_REPRESENTATION);
+        return new Piece(WHITE, WHITE_ROOK_REPRESENTATION);
     }
 
     public static Piece createBlackRook() {
-        return new Piece(BLACK_COLOR, BLACK_ROOK_REPRESENTATION);
+        return new Piece(BLACK, BLACK_ROOK_REPRESENTATION);
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(WHITE_COLOR, WHITE_BISHOP_REPRESENTATION);
+        return new Piece(WHITE, WHITE_BISHOP_REPRESENTATION);
     }
 
     public static Piece createBlackBishop() {
-        return new Piece(BLACK_COLOR, BLACK_BISHOP_REPRESENTATION);
+        return new Piece(BLACK, BLACK_BISHOP_REPRESENTATION);
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(WHITE_COLOR, WHITE_QUEEN_REPRESENTATION);
+        return new Piece(WHITE, WHITE_QUEEN_REPRESENTATION);
     }
 
     public static Piece createBlackQueen() {
-        return new Piece(BLACK_COLOR, BLACK_QUEEN_REPRESENTATION);
+        return new Piece(BLACK, BLACK_QUEEN_REPRESENTATION);
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(WHITE_COLOR, WHITE_KING_REPRESENTATION);
+        return new Piece(WHITE, WHITE_KING_REPRESENTATION);
     }
 
     public static Piece createBlackKing() {
-        return new Piece(BLACK_COLOR, BLACK_KING_REPRESENTATION);
+        return new Piece(BLACK, BLACK_KING_REPRESENTATION);
     }
 
     public boolean isWhite() {
-        return (Objects.equals(color, WHITE_COLOR));
+        return (Objects.equals(color, WHITE));
     }
 
     public boolean isBlack() {
-        return (Objects.equals(color, BLACK_COLOR));
+        return (Objects.equals(color, BLACK));
     }
 
     /**
      * black 또는 white 색상이 아닌 경우 예외를 던짐
      * TODO: 던진 예외를 처리해야 함
      */
-    private void validatePiece(final String color, final char representation) {
-        if (!color.equals(BLACK_COLOR) && !color.equals(WHITE_COLOR)
+    private void validatePiece(final Color color, final char representation) {
+        if (!color.equals(BLACK) && !color.equals(WHITE)
                 && representation != BLACK_PAWN_REPRESENTATION && representation != WHITE_PAWN_REPRESENTATION) {
             throw new IllegalArgumentException("[ERROR] color: " + color + ", " + "representation: " + representation);
         }
--- src/test/java/pieces/PieceTest.java
+++ src/test/java/pieces/PieceTest.java
@@ -9,11 +9,11 @@ class PieceTest {
     @DisplayName("흰색 폰과 검은색 폰만이 생성되어야 한다")
     @Test
     void create_piece() {
-        validatePiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
-        validatePiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+        validatePiece(Piece.createWhitePawn(), Piece.Color.WHITE, Piece.WHITE_PAWN_REPRESENTATION);
+        validatePiece(Piece.createBlackPawn(), Piece.Color.BLACK, Piece.BLACK_PAWN_REPRESENTATION);
     }
 
-    private void validatePiece(final Piece piece, final String color, final char representation) {
+    private void validatePiece(final Piece piece, final Piece.Color color, final char representation) {
         assertThat(color).isEqualTo(piece.getColor());
         assertThat(representation).isEqualTo(piece.getRepresentation());
     }
